c8d3c08ee47d0bc0d72c7f027de3313c520da1c9
anzhen <anzhen@play4u.cn>
2026-06-04 18:16:09 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-cd /Users/anzhen/go/src/ai-test/packages/web
+cd /Users/anzhen/publicGit/ai-test/packages/web
 npm run build
 
-rsync -rv --exclude='.git' --exclude='node_modules' --delete -e 'ssh -p 22 -o ServerAliveInterval=10 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null' /Users/anzhen/go/src/ai-test root@39.106.88.210:/root/
+rsync -rv --exclude='.git' --exclude='node_modules' --delete -e 'ssh -p 22 -o ServerAliveInterval=10 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null' /Users/anzhen/publicGit/ai-test root@39.106.88.210:/root/
